d3f96c750a4d40accd1787329e33a8a4666437ef
JoelFC45 <joelfelip45@gmail.com>
2025-10-17 16:33:38 -0300

    Funções básicas Javascript

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function addNumber(number) {
+    document.querySelector('#display').value += number;
     console.log(number);
+}
+function erase() {
+   let tela = document.querySelector('#display').value;
+   document.querySelector('#display').value = tela.slice(0, -1);
+}
+function eraseAll(){
+    document.querySelector('#display').value = "";
+}
+function doting(){
+    document.querySelector('#display').value += ".";
 }
