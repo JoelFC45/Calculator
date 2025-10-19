54814fdcd48ba80e530782df9c6120eee5ff1a0b
JoelFC45 <joelfelip45@gmail.com>
2025-10-18 21:01:57 -0300

    Tornando as operações funcionais

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 function addNumber(number) {
-    document.querySelector('#display').value += number;
+  document.querySelector("#display").value += number;
 }
+var num1 = null;
+var num2 = null;
+var operator = null;
 function erase() {
-   let tela = document.querySelector('#display').value;
-   document.querySelector('#display').value = tela.slice(0, -1);
+  let tela = document.querySelector("#display").value;
+  document.querySelector("#display").value = tela.slice(0, -1);
 }
-function eraseAll(){
-    document.querySelector('#display').value = "";
+function eraseAll() {
+  document.querySelector("#display").value = "";
 }
-function doting(){
-    document.querySelector('#display').value += ".";
+function doting() {
+  if (document.querySelector("#display").value == null) {
+    document.querySelector("#display").value = "0.";
+  } else {
+    document.querySelector("#display").value += ".";
+  }
 }
-function transform(){
-    let operador = document.querySelector('#display').value;
-    parseFloat(operador); 
+
+function transform(num) {
+  if (num1 === null) {
+    num1 = parseFloat(num);
+    return num1;
+  } else {
+    num2 = parseFloat(num);
+    return num2;
+  }
+}
+function chooseOperator(op) {
+  let num = document.querySelector("#display").value;
+  transform(num);
+  document.querySelector("#display").value = "";
+  operator = op;
+  return operator;
+}
+
+function calculate() {
+  switch (operator) {
+    case "+":
+      document.querySelector("#display").value = num1 + num2;
+      break;
+    case "-":
+      document.querySelector("#display").value = num1 - num2;
+      break;
+    case "*":
+      document.querySelector("#display").value = num1 * num2;
+      break;
+    case "/":
+      document.querySelector("#display").value = num1 / num2;
+      break;
+  }
 }
